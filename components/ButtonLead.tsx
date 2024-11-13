2165e105872e31e68c028a7b051fb7d1ab2894c7
TylerMaschoff <66757633+Tymash55@users.noreply.github.com>
2024-11-12 23:09:10 -0600

    fix bugs

--- components/ButtonLead.tsx
+++ components/ButtonLead.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import apiClient from "@/libs/api";
 import React, { useState, useRef } from "react";
 import { toast } from "react-hot-toast";
-import apiClient from "@/libs/api";
 
 // This component is used to collect the emails from the landing page
 // You'd use this if your product isn't ready yet or you want to collect leads
